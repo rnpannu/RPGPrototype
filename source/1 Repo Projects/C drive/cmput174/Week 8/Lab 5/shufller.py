f92855fe23c89f73a992d1f78c196d9df5ded5e5
Manraj Pannu <manrajpannu2003@gmail.com>
2026-08-18 12:51:21 -0600

    Update execution order changes, refactored into think->check->move system

--- source/1 Repo Projects/C drive/cmput174/Week 8/Lab 5/shufller.py
+++
@@ -1,4 +0,0 @@
-import random
-list1 = ['skoop', 'bblaaa', 'what', 'done']
-random.shuffle(list1)
-print(list1)
--- source/1 Repo Projects/C drive/cmput174/untitled-1.py
+++
@@ -1,3 +0,0 @@
-matri = [[2,3,4],[4,5,6],[6,7,8]]
-for row in matri:
-    print(row[0])
